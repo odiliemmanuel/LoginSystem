5b5d4cd8b5478716cfe59e6fa1cf6ec745c95013
odiliemmanuel <your-github-odiliejeh09@gmail.com>
2026-03-30 17:56:27 +0100

    "project"

--- src/main/java/org/loginsystem/exceptions/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package org.loginsystem.exceptions;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException(String message) {
-        super(message);
-    }
-}
